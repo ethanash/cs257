d8a7f16a783644008bbe4926847eda478b1bcfdf
Ethan Ash <ethanash@ethans-air.lan>
2021-01-08 08:55:05 -0800

    final edits for the python file

--- hello/hello.py
+++ hello/hello.py
@@ -1,4 +1,6 @@
-print('hello world!')
-print("taco")
+#Made by Quoc Nguyen and Ethan Ash
+print('By: Quoc Nguyen and Ethan Ash')
+print('Hello Wonderful Grader!')
+print("We hope you are having a nice day!")
 
 exit()

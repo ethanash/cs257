ce82b18c16d0d13c56afa22fcbb175cca3bd8df6
Ethan Ash <ashe@carleton.edu>
2021-01-26 12:19:33 -0600

    edits to queries

--- database/queries.sql
+++ database/queries.sql
@@ -7,24 +7,24 @@ ORDER BY noc;
 
 --List the names of all the athletes from Kenya. If your database design allows it, sort the athletes by last name.
 SELECT athletes.athlete 
-FROM athletes 
+FROM athletes, athletes_teams, teams
 WHERE athletes_teams.athlete_id = athletes.id 
-AND athletes_teams.team_id = (teams.id WHERE teams.team = "Kenya");
+AND athletes_teams.team_id = teams.id AND teams.team = 'Kenya';
 
 --List all the medals won by Greg Louganis, sorted by year. Include whatever fields in this output that you think appropriate.
-SELECT medals.medal 
-FROM medals 
-WHERE (medals.id = event_performances.medal_id 
-AND event_performances.athlete_id = (athletes.id 
-where athletes.athlete = "%Greg Louganis%") 
-ORDER BY games.year 
-WHERE games)id = event_performances.game_id );
+SELECT event_performances, medals.medal 
+FROM medals, event_performances, athletes, games
+WHERE athletes.athlete = '%A Dijiang%'
+AND medals.id = event_performances.medal_id
+AND event_performances.athlete_id = athletes.id 
+AND games.id = event_performances.game_id
+ORDER BY games.year;
 
 --List all the NOCs and the number of gold medals they have won, in decreasing order of the number of gold medals.
 SELECT nocs.noc, COUNT(event_performances.medal_id) 
 FROM nocs, medals 
 WHERE event_performances.medal_id = medals.id 
-AND (WHERE medals.medal = "Gold") 
+AND (WHERE medals.medal = 'Gold') 
 GROUP BY nocs.noc WHERE nocs.id = teams.noc_id 
 (WHERE teams.id = event_performance.team_id) 
 ORDER BY count(event_performances.medal_id) DESC;

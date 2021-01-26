0c5c0d90c35e585b63d3a5ade5a0830ca5750d7a
matthewse19 <mcse@maine.rr.com>
2021-01-26 16:00:59 -0600

    revised the query for NOCs gold count

--- database/queries.sql
+++ database/queries.sql
@@ -1,17 +1,18 @@
+--Ethan Ash, Matthew Smith-Erb
 --Queries:
 
---List all the NOCs (National Olympic Committees), in alphabetical order by abbreviation. These entities, by the way, are mostly equivalent to countries. But in some cases, you might find that a portion of a country participated in a particular games (e.g. one guy from Newfoundland in 1904) or some other oddball situation.
+--List all the NOCs (National Olympic Committees), in alphabetical order by abbreviation.
 SELECT noc
 FROM nocs
 ORDER BY noc;
 
---List the names of all the athletes from Kenya. If your database design allows it, sort the athletes by last name.
+--List the names of all the athletes from Kenya
 SELECT athletes.athlete
 FROM athletes, athletes_teams, teams
 WHERE athletes_teams.athlete_id = athletes.id
 AND athletes_teams.team_id = teams.id AND teams.team = 'Kenya';
 
---List all the medals won by Greg Louganis, sorted by year. Include whatever fields in this output that you think appropriate.
+--List all the medals won by Greg Louganis, sorted by year
 SELECT events.event, games.year, medals.medal
 FROM medals, event_performances, athletes, games, events
 WHERE athletes.athlete LIKE '%Louganis%'
@@ -23,9 +24,10 @@ ORDER BY games.year;
 
 --List all the NOCs and the number of gold medals they have won, in decreasing order of the number of gold medals.
 SELECT nocs.noc, COUNT(event_performances.medal_id)
-FROM nocs, medals
-WHERE event_performances.medal_id = medals.id
-AND (WHERE medals.medal = 'Gold')
-GROUP BY nocs.noc WHERE nocs.id = teams.noc_id
-(WHERE teams.id = event_performance.team_id)
-ORDER BY count(event_performances.medal_id) DESC;
+FROM nocs, medals, event_performances, teams
+WHERE medals.medal = 'Gold'
+AND event_performances.medal_id = medals.id
+AND teams.id = event_performances.team_id
+AND teams.noc_id = nocs.id
+GROUP BY nocs.noc
+ORDER BY COUNT(event_performances.medal_id) DESC;
